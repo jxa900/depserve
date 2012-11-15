8e135565db7f7ee08d07578c5bb4644137b86373
Michael Chapman <woppin@gmail.com>
2012-11-15 13:45:18 +1100

    added gunicorn hooks

--- depserve.py
+++ depserve.py
@@ -12,8 +12,10 @@
 
 import os
 from flask import Flask, render_template, request, jsonify
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 class Host():
     def __init__(self, name, mac, ip):
