4d60c2159cda8f283c532bddb2027d0c879dc037
Ubuntu <ubuntu@flasktest.novalocal>
2012-11-15 03:09:43 +0000

    Added BSD license and a simple setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""
+flask-depserve
+--------
+
+Handles Kickstarting VMs in the dhcpd.conf file and presenting the dhcpd.conf file as JSON for other services to consume
+"""
+
+from setuptools import setup
+
+setup(
+    name='flask-depserve',
+    version='0.1.0',
+    url='https://github.com/ncicloudteam/depserve',
+    license='BSD',
+    author='Michael Chapman',
+    author_email='michael.chapman@anu.edu.au',
+    description='Handles Kickstarting VMs in the dhcpd.conf file and presenting the dhcpd.conf file as JSON for other services to consume',
+    long_description=__doc__,
+    py_modules=['depserve'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['Flask'],
+)
